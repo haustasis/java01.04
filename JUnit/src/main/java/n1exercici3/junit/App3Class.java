1e483df45ac3a63de6e13c15585302718e0d2bd7
4SH1N <20546366+haustasis@users.noreply.github.com>
2022-02-15 09:24:07 +0100

    enunciats

---
+++ JUnit/src/main/java/n1exercici3/junit/App3Class.java
@@ -0,0 +1,7 @@
+package n1exercici3.junit;
+/*
+Exercici 3. Crea una classe amb un mètode que llanci una ArrayIndexOutOfBoundsException.
+ Verifica el seu correcte funcionament amb un test jUnit.
+ */
+public class App3Class {
+}
